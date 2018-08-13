ff26fc73e27f7d5a41f741e5b2fe6b3f3e9e0b16
fengyuexingzi <fengyuexingzi@gmail.com>
2018-08-13 17:22:52 +0800

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,7 @@
         </style>
     </head>
     <body>
+        <div class="flex-center position-ref full-height">
 
             <div class="content">
                 <div class="title m-b-md">
